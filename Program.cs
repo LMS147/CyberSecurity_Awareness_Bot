05c54643ecbc243ff2ec5f1d9509a7e429211e57
LMS147 <mashaoleshaolin0@gmail.com>
2025-03-31 19:08:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+using System.Threading;
+
+
+namespace CyberSecurity_Awareness_Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
